551c7d81545b6840ffa7255ca69a1f3d2551eea0
Tejal Waghe <delululoser3@gmail.com>
2026-04-01 00:02:42 +0530

    Add files via upload

---
+++ cloudinary_config.php
@@ -0,0 +1,15 @@
+<?php
+require 'vendor/autoload.php';
+
+use Cloudinary\Configuration\Configuration;
+
+Configuration::instance([
+  'cloud' => [
+    'cloud_name' => 'dnry8qnk',
+    'api_key'    => '322683191428718',
+    'api_secret' => '1_VL0YL_ynozE9yBxGaThCGdJiw'
+  ],
+  'url' => [
+    'secure' => true
+  ]
+]);
