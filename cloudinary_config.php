7b15e42d70b408ca653cfc294d48b3a1bb9c834e
Tejal Waghe <delululoser3@gmail.com>
2026-04-01 01:33:26 +0530

    Update cloudinary_config.php

--- cloudinary_config.php
+++ cloudinary_config.php
@@ -5,7 +5,7 @@ use Cloudinary\Configuration\Configuration;
 
 Configuration::instance([
   'cloud' => [
-    'cloud_name' => 'dnry8qnk',
+    'cloud_name' => 'dnry8qnkk',
     'api_key'    => '322683191428718',
     'api_secret' => '1_VL0YL_ynozE9yBxGaThCGdJiw'
   ],
